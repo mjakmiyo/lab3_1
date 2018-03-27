43072606a34dd0531e8693ac11d70af885ad6928
Monika <jedrzejekmonika@gmail.com>
2018-03-27 18:03:00 +0200

    Init BookKeeperTest

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,15 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BookKeeperTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
