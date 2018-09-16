b07c043e3a1a527d120481a07016d90cadbee0c7
mjakmiyo <jedrzejekmonika@gmail.com>
2018-09-16 19:16:11 +0200

    Add calculateTax behaviour test

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,12 +4,15 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -30,8 +33,21 @@ public class BookKeeperTest {
 	public void invoiceRequestWithOneItemShouldReturnInvoiceWithOneItem() {
 		requestItem = new RequestItem(productData, 1, new Money(1));
 		invoiceRequest.add(requestItem);
+
 		final Invoice issuedInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
 		assertThat(issuedInvoice.getItems().size(), is(1));
+	}
+
+	@Test
+	public void invoiceRequestWithTwoItemsShouldInvokeCalculateTaxMethodTwice() {
+		requestItem = new RequestItem(productData, 1, new Money(1));
+
+		invoiceRequest.add(requestItem);
+		invoiceRequest.add(requestItem);
+
+		bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+		verify(taxPolicy, times(2)).calculateTax(Mockito.<ProductType>any(), Mockito.<Money>any());
 	}
 
 	@Before
@@ -39,9 +55,12 @@ public class BookKeeperTest {
 		invoiceFactory = mock(InvoiceFactory.class);
 		when(invoiceFactory.create(any(ClientData.class)))
 				.thenReturn(new Invoice(Id.generate(), new ClientData(Id.generate(), "Test")));
+
 		bookKeeper = new BookKeeper(invoiceFactory);
 		productData = new ProductData(Id.generate(), new Money(1), "Test", ProductType.STANDARD, new Date());
+		clientData = new ClientData(Id.generate(), "Test");
 		invoiceRequest = new InvoiceRequest(clientData);
+
 		taxPolicy = mock(TaxPolicy.class);
 		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(1), "Tax"));
 	}
